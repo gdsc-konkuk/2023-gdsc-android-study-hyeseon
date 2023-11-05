cd6d21ab841b36c941ab572e4617507d04fc9ed9
HyeseonBaek <konkukbhs@gmail.com>
2023-11-05 12:47:00 +0900

    [#3/fix] 뷰 중복 문제 해결

--- app/src/main/java/kr/ac/konkuk/gdsc/hyeseon/presentation/MainActivity.kt
+++ app/src/main/java/kr/ac/konkuk/gdsc/hyeseon/presentation/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         navigateTo<HomeFragment>()
         initBnvItemSelectedListener()
         syncBottomNavigationBackStackAdded()
