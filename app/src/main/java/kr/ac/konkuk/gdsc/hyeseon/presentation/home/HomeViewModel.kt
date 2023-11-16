74fd44c1e3827c43c84330e1816b702501919a2f
HyeseonBaek <konkukbhs@gmail.com>
2023-11-17 03:37:28 +0900

    [#6/feat] todo 데이터 room 저장

--- app/src/main/java/kr/ac/konkuk/gdsc/hyeseon/presentation/home/HomeViewModel.kt
+++ app/src/main/java/kr/ac/konkuk/gdsc/hyeseon/presentation/home/HomeViewModel.kt
@@ -1,28 +1,44 @@
 package kr.ac.konkuk.gdsc.hyeseon.presentation.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import kr.ac.konkuk.gdsc.hyeseon.domain.entity.TodoItem
-import kr.ac.konkuk.gdsc.hyeseon.domain.entity.TodoItem.Companion.DummyList
+import kr.ac.konkuk.gdsc.hyeseon.domain.repository.TodoRepository
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(): ViewModel() {
-    private val _todoList = MutableStateFlow(DummyList)
+class HomeViewModel @Inject constructor(
+    private val todoRepository: TodoRepository
+): ViewModel() {
+    private val _todoList = MutableStateFlow<List<TodoItem>>(emptyList())
     val todoList: StateFlow<List<TodoItem>> = _todoList
 
-    fun updateDoneBtn(item: TodoItem) {
-        val updatedList = _todoList.value.map { todoItem ->
-            if (todoItem.id == item.id) {
-                todoItem.copy(isDone = !todoItem.isDone)
-            } else {
-                todoItem
-            }
+    init {
+        viewModelScope.launch {
+            _todoList.value = todoRepository.getAllTodos().first().map { it }
         }
-        _todoList.value = updatedList
     }
+
+    fun updateDoneBtn(item: TodoItem) {
+        viewModelScope.launch {
+            val updatedItem = item.copy(isDone = !item.isDone)
+            todoRepository.updateTodo(updatedItem)
+            val updatedList = _todoList.value.map { todoItem ->
+                if (todoItem.id == item.id) {
+                    updatedItem
+                } else {
+                    todoItem
+                }
+            }
+            _todoList.value = updatedList
+        }
+    }
+
 
     fun getToDoDoneCount(): Int {
         return _todoList.value.count { it.isDone }
